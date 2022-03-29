17b0b518caa1af4678157710ba8d0849903736ec
Ola Andersson <ola.andersson@tretton37.com>
2022-03-29 15:38:23 +0200

    Updated with working css shift

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /// <reference types="@fastly/js-compute" />
 import indexPage from "./index.html";
+import baseStyle from "./style.css";
+import darkStyle from "./dark.css";
+import lightStyle from "./light.css";
 
 // https://github.com/w3ctag/design-reviews/issues/632
 // https://web.dev/user-preference-media-features-headers/
@@ -10,33 +13,41 @@ async function handleRequest(event) {
   const req = event.request;
   const prefersColorScheme = req.headers.get("Sec-CH-Prefers-Color-Scheme");
 
-  // console.log(prefersColorScheme);
+  console.log(prefersColorScheme);
 
   const url = new URL(req.url);
-  const pathname = url.pathname;
-  const isCssResource = url.pathname.endsWith(".css");
+  console.log(url.pathname);
+
+  const headers = new Headers({
+    "ACCEPT-CH": "Sec-CH-Prefers-Color-Scheme",
+    "CRITICAL-CH": "Sec-CH-Prefers-Color-Scheme",
+    VARY: "Sec-CH-Prefers-Color-Scheme",
+  });
 
   if (url.pathname == "/") {
+    headers.set("Content-Type", "text/html; charset=utf-8");
     return new Response(indexPage, {
       status: 200,
-      headers: new Headers({
-        "Content-Type": "text/html; charset=utf-8",
-        "ACCEPT-CH": "Sec-CH-Prefers-Color-Scheme",
-        "CRITICAL-CH": "Sec-CH-Prefers-Color-Scheme",
-        VARY: "Sec-CH-Prefers-Color-Scheme",
-      }),
+      headers,
     });
-  }
-
-  if (isCssResource) {
-    const resource = pathname.split(".")[1];
-    console.info(`RESOURCE ${resource}`);
-
+  } else if (url.pathname.endsWith("style.css")) {
     headers.set("Content-Type", "text/css; charset=utf-8");
-
-    return new Response("hello", {
+    return new Response(baseStyle, {
+      status: 200,
+      headers,
+    });
+  } else if (url.pathname.endsWith("color-scheme.css")) {
+    console.info(`Prefers color scheme: ${prefersColorScheme}`);
+    let style = prefersColorScheme == "dark" ? darkStyle : lightStyle;
+    headers.set("Content-Type", "text/css; charset=utf-8");
+    return new Response(style, {
       status: 200,
       headers,
     });
   }
+
+  // Catch all other requests and return a 404.
+  return new Response("The page you requested could not be found", {
+    status: 404,
+  });
 }
